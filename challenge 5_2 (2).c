dce7e24b164e131e653cf5443651990798c40a69
khaouti nouhaila <khaoutinouhaila@gmail.com>
2022-09-09 17:11:51 +0100

    challenge1&2

--- challenge 5_2 (2).c
+++ challenge 5_2 (2).c
@@ -4,13 +4,14 @@ int main(){
      int i;
      for(i=1;i<=100;i++){
          if(i%3==0){
-            printf("fizz \n");
+            printf("fizz");
          }
          if(i%5==0){
-            printf("buzz \n");
+            printf("buzz");
          }
          if(i%3!=0 && i%5!=0){
-            printf(" %d \n",i);
+            printf(" %d ",i);
          }
+         printf("\n");
      }
     }
---
+++ challenge 6_2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int i, j, n, k = 0;
+    char Mot1[10], Mot2[10];
+
+    printf("***********\n");
+    printf("entrer un mots qui se compose de 4 caractere et ne contient que les caracteres suivantes 'a,b,c,d' \n");
+    scanf("%[^\n]%*c", &Mot1);
+    printf("entrer un autre mots avec les memes condition \n");
+    scanf("%[^\n]%*c", &Mot2);
+    printf("***********\n");
+    for (i = 0; i < strlen(Mot1); i++)
+    {
+        if (Mot1[i] != ' ')
+        {
+            for (j = 0; j < strlen(Mot2); j++)
+            {
+                    if (Mot1[i] == Mot2[j])
+                    {
+                        n++;
+                        break;
+                    }
+            }
+        }
+        else
+        {
+            k++;
+        }
+    }
+    if (n == strlen(Mot1) - k)
+    {
+        printf("les deux mots sont anagramme\n");
+    }
+    else
+    {
+        printf("les deux mots ne sont pas anagramme\n");
+    }
+}
