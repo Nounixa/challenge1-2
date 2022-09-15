6ee71c69e42c90553ae39882705977d463466cd0
nouhaila_khaouti <96340163+Nounixa@users.noreply.github.com>
2022-09-15 01:06:32 +0100

    Update etoile.c

--- etoile.c
+++ etoile.c
@@ -8,20 +8,25 @@ int main(){
 
         for ( i = a; i != 0; i--)
         {  
-            for (k = 0 ; k<i ; k++)
-           {
-               printf(" "); 
-           }
-           for (j = 0 ; j <= a-i; j++)
-           {
-            printf("*"); 
-           }
-           for (j = 0 ; j <= (a-i-1); j++)
-           {
-            printf("*"); 
-           }
+            for (k = 0 ; k<=i ; k++)
+          {
+              printf(" "); 
+          }
+          for (j = 0 ; j <= a-i; j++)
+          {
+            printf("* "); 
+          }
+        //   for (j = 0 ; j <= (a-i-1); j++)
+        //   {
+        //     printf("*"); 
+        //   }
+        //     for (k = 0 ; k!=i ; k++)
+        //   {
+        //       printf(" *"); 
+        //   }
             printf("\n");
-         }          
+         }     
+
         
     return 0;
 }
